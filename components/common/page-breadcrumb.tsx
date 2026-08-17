3ba2279e31b3af97c771580345ca6e90450e5c29
caesariomj <caesariomj7@gmail.com>
2026-08-17 16:23:14 +0700

    feat(ui): add page-breadcrumb component

---
+++ components/common/page-breadcrumb.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Fragment } from "react";
+import { usePathname } from "next/navigation";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function PageBreadcrumb() {
+  const pathname = usePathname();
+  const breadcrumbItems = pathname.split("/").filter(Boolean);
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {breadcrumbItems.map((item, index) => {
+          const href = "/" + breadcrumbItems.slice(0, index + 1).join("/");
+          const isLast = index === breadcrumbItems.length - 1;
+
+          return (
+            <Fragment key={item}>
+              <BreadcrumbItem>
+                {isLast ? (
+                  <BreadcrumbPage>{item}</BreadcrumbPage>
+                ) : (
+                  <BreadcrumbLink href={href} className="underline">
+                    {item}
+                  </BreadcrumbLink>
+                )}
+              </BreadcrumbItem>
+              {!isLast && <BreadcrumbSeparator />}
+            </Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
